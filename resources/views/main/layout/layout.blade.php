9c9aeddc3ccbd59be84bec472a0cce2fc6722d9c
YegorChevardin <egor03052004@gmail.com>
2020-05-16 22:28:36 +0300

    version 0.0003

---
+++ resources/views/main/layout/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>Flattern Bootstrap Template - Index</title>
+        <meta name="author" content="Yegor Chevardin"/>
+        <meta name="keywords" content=""/>
+        <meta name="description" content="Our home web page that helps take care about our guests"/>
+        @include('main.layout.header_css')
+    </head>
+    <body>
+        @include('main.layout.header')
+        @yield('content')
+        @include('main.layout.footer')
+        <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+        @include('main.layout.footer_scripts')
+    </body>
+</html>
